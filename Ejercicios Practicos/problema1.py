80910e2e5032639e58efdb3923c369f86c75c170
olveraSharon <144488383+olveraSharon@users.noreply.github.com>
2023-09-25 11:53:58 -0600

    Add files via upload
    Ejercicios precticos

---
+++ Ejercicios Practicos/problema1.py
@@ -0,0 +1,20 @@
+#Autor: Sharon Michelle Olvera Ibarra
+#Descripcion: Una panadería vende barras de pan a $3.49 cada una. El pan que no es el día tiene un descuento del 60%. 
+#Escribe un programa que comience leyendo el número de barras vendidas que no son del día. Después tu programa debe 
+#mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste final total.
+#Fecha: 25/09/2023
+n= input()
+m= 3.49-0.6
+panViejo = int(input())
+print("¡Cuantos panes no son del dia?")
+
+if n :
+    print("el pan cuesta $3.49")
+else :
+    m= 3.49-0.6
+    print("El precio de pan con descuento es" (m))
+
+
+
+
+
